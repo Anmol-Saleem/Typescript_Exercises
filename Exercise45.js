d8d5a4bbf275b81efa28a526657b18aada2e4681
ANMOL SALEEM <anmolsaleem661@gmail.com>
2024-05-11 23:06:27 +0500

    Add files via upload

---
+++ Exercise45.js
@@ -0,0 +1,5 @@
+"use strict";
+// Exercise 45
+// Install Node.js, TypeScript and VS Code on your computer.
+/* This exercise is actually exercise 01 which i have already completed so my 45 exercises are completed.
+*/ 
